b2e137a8f5b5dd93674706d4a897ae4af07e1668
Ahmed-Raed-Siam <68457689+Ahmed-Raed-Siam@users.noreply.github.com>
2020-12-02 15:53:19 +0200

    add DataTables css and js to master.blade.php

--- resources/views/dashboard/layout/master.blade.php
+++ resources/views/dashboard/layout/master.blade.php
@@ -12,9 +12,13 @@
     <!-- Font Awesome -->
     <link rel="stylesheet" href="{{ asset("plugins/fontawesome-free/css/all.min.css") }}">
     <!-- overlayScrollbars -->
-    <link rel="stylesheet" href="plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+    <link rel="stylesheet" href="{{ asset("plugins/overlayScrollbars/css/OverlayScrollbars.min.css") }}">
+    <!-- DataTables -->
+    <link rel="stylesheet" href="{{ asset("plugins/datatables-bs4/css/dataTables.bootstrap4.min.css") }}">
+    <link rel="stylesheet" href="{{ asset("plugins/datatables-responsive/css/responsive.bootstrap4.min.css") }}">
+    <link rel="stylesheet" href="{{ asset("plugins/datatables-buttons/css/buttons.bootstrap4.min.css") }}">
     <!-- Theme style -->
-    <link rel="stylesheet" href="dist/css/adminlte.min.css">
+    <link rel="stylesheet" href="{{ asset("dist/css/adminlte.min.css") }}">
     <!-- Ionicons -->
     <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
     <!-- Tempusdominus Bootstrap 4 -->
@@ -57,6 +61,19 @@
 </script>
 <!-- Bootstrap 4 -->
 <script src="{{ asset("plugins/bootstrap/js/bootstrap.bundle.min.js") }}"></script>
+<!-- DataTables  & Plugins -->
+<script src="{{ asset("plugins/datatables/jquery.dataTables.min.js") }}"></script>
+<script src="{{ asset("plugins/datatables-bs4/js/dataTables.bootstrap4.min.js") }}"></script>
+<script src="{{ asset("plugins/datatables-responsive/js/dataTables.responsive.min.js") }}"></script>
+<script src="{{ asset("plugins/datatables-responsive/js/responsive.bootstrap4.min.js") }}"></script>
+<script src="{{ asset("plugins/datatables-buttons/js/dataTables.buttons.min.js") }}"></script>
+<script src="{{ asset("plugins/datatables-buttons/js/buttons.bootstrap4.min.js") }}"></script>
+<script src="{{ asset("plugins/jszip/jszip.min.js") }}"></script>
+<script src="{{ asset("plugins/pdfmake/pdfmake.min.js") }}"></script>
+<script src="{{ asset("plugins/pdfmake/vfs_fonts.js") }}"></script>
+<script src="{{ asset("plugins/datatables-buttons/js/buttons.html5.min.js") }}"></script>
+<script src="{{ asset("plugins/datatables-buttons/js/buttons.print.min.js") }}"></script>
+<script src="{{ asset("plugins/datatables-buttons/js/buttons.colVis.min.js") }}"></script>
 <!-- ChartJS -->
 <script src="{{ asset("plugins/chart.js/Chart.min.js") }}"></script>
 <!-- Sparkline -->
