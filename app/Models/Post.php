9db2ff23cd402ca394c63d1bb82c886333567a3d
Ahmed-Raed-Siam <68457689+Ahmed-Raed-Siam@users.noreply.github.com>
2020-12-07 19:11:40 +0200

    Add Relationship to post model Post.php with comments

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,19 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use App\Models\Category;
-use App\Models\Comment;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Post extends Model
 {
     use HasFactory;
 
-    public function category(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category(): BelongsTo
     {
         return $this->belongsTo(Category::class);
     }
 
-    public function comments(): \Illuminate\Database\Eloquent\Relations\HasMany
+    public function comments(): HasMany
     {
         return $this->hasMany(Comment::class);
     }
