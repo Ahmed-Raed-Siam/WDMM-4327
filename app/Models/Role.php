b2e74baa018287e1b24b4c114b20d3ce6f865c8d
Ahmed-Raed-Siam <68457689+Ahmed-Raed-Siam@users.noreply.github.com>
2020-12-11 10:51:16 +0200

    Add users relationship(Many to Many) to Role.php with user model User.php

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -4,8 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Role extends Model
 {
     use HasFactory;
+
+    public function users(): BelongsToMany
+    {
+//        return $this->belongsToMany(User::class, 'role_users', 'role_id', 'user_id');
+        return $this->belongsToMany(User::class, 'role_users');
+    }
 }
