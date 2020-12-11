1e206a9b3b2a99b1fff4b53ea6d42460abe4010d
Ahmed-Raed-Siam <68457689+Ahmed-Raed-Siam@users.noreply.github.com>
2020-12-11 10:50:27 +0200

    Add roles relationship(Many to Many) to User.php with role model Role.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,7 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Concerns\HasTimestamps;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -11,7 +13,7 @@ use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable, SoftDeletes;
+    use HasFactory, Notifiable, HasTimestamps, SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
@@ -43,9 +45,23 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
+    /**
+     * @return HasMany
+     */
     public function posts(): HasMany
     {
         return $this->hasMany(Post::class);
+    }
+
+    /**
+     * @return BelongsToMany
+     */
+    public function roles(): BelongsToMany
+    {
+//        return $this->hasMany(Role::class);
+//        return $this->belongsToMany(Role::class);
+//        return $this->belongsToMany(Role::class, 'role_users', 'user_id', 'role_id');
+        return $this->belongsToMany(Role::class, 'role_users');
     }
 
 }
